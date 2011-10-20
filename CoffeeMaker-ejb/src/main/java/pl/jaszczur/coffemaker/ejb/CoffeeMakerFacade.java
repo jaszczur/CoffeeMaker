92dfbcb2125f94a49dee0d374b74b20dad270718
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2011-10-20 20:44:00 +0200

    db

--- CoffeeMaker-ejb/src/main/java/pl/jaszczur/coffemaker/ejb/CoffeeMakerFacade.java
+++ CoffeeMaker-ejb/src/main/java/pl/jaszczur/coffemaker/ejb/CoffeeMakerFacade.java
@@ -5,6 +5,8 @@
 package pl.jaszczur.coffemaker.ejb;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import pl.jaszczur.coffemaker.domain.Coffee;
 import pl.jaszczur.coffemaker.domain.CoffeeMaker;
 
@@ -15,10 +17,14 @@ import pl.jaszczur.coffemaker.domain.CoffeeMaker;
 @Stateless
 public class CoffeeMakerFacade implements CoffeeMakerFacadeLocal {
     private CoffeeMaker coffeeMaker = new CoffeeMaker();
+    
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public void makeCoffee(Coffee c) {
         System.err.println("Making a coffee");
+        em.persist(c);
         coffeeMaker.makeACoffee(c);
     }
 
